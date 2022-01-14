a1cea5d65862555256451fb69ab4e9bcaea26a5c
antonio <tonyantonio@outlook.pt>
2022-01-14 09:58:11 +0000

    all work

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import time
 import nltk # install pip install nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+results_exact_file = "results_exact.csv"
+results_ssc_file = "results_ssc_count.csv"
 
 def available_books():
     bible = "books/bible.txt"
@@ -23,24 +24,21 @@ def available_books():
         nltk.download('punkt')
         stop_words_eng = set(nltk.corpus.stopwords.words('english'))
         stop_words_pt = set(nltk.corpus.stopwords.words('portuguese'))
+        stop_words_sp = set(nltk.corpus.stopwords.words('spanish'))
 
         print("\n\nChoose a book between the followings \n")
         print(f"0 - {bible}")
         print(f"1 - {maias}")
         print(f"2 - {quijote}")
-        print(f"3 - {test_eng}")
-        print(f"4 - {test_port}")
+
         choice = int(input())
         if choice == 0: 
             return bible, stop_words_eng
         if choice == 1:
             return maias, stop_words_pt
         if choice == 2:
-            return quijote
-        if choice == 3:
-            return test_eng, stop_words_eng
-        if choice == 4:
-            return test_port, stop_words_pt
+            return quijote, stop_words_sp
+
     except ValueError:
         print("Please input integer only...")  
 
@@ -91,16 +89,31 @@ def space_saving_count(words, k):
     return ssc_count, round(stop, 3)
 
 def create_results_file():
-    file = open("results.csv", "w")
-    file.write("Word, Exact_Count, Space_Saving_Count\n")
+    file = open(results_exact_file, "w")
+    file.write("Word, Count, Time_exc\n")
+    file.close()
+    file_ssc_count = open(results_ssc_file, "w")
+    file_ssc_count.write("Word, Count, Time_exc\n")
+    file_ssc_count.close
+
+def write_results(exact_count, time_exc_count):
+    """ write results of exact count in new file """
+    file = open(results_exact_file, "a")
+
+    exact_count = dict(sorted(exact_count.items(),key=lambda x:x[0],reverse = False))
+    for word in exact_count:
+        file.write(f"{word}, {exact_count[word]}, {time_exc_count}\n")
     file.close()
 
-def write_results(words, exact_count, time_exc_count, ssc_count, time_ssc_count):
-    file = open("results.csv", "a")
-    for word in words:
-        if word in exact_count and word in ssc_count:
-            file.write(f"{word}, {exact_count[word]}, {time_exc_count}, {ssc_count[word]}, {time_ssc_count}\n")
+
+def write_results_ssc_count(ssc_count, time_ssc_count, k):
+    """ write results of ssc_count and k in new file """
+    file = open(results_ssc_file, "a")
+    ssc_count = dict(sorted(ssc_count.items(),key=lambda x:x[0],reverse = False))
+    for word in ssc_count:
+        file.write(f"{word}, {ssc_count[word]}, {time_ssc_count}, {k}\n")
     file.close()
+
 
 if __name__ == "__main__":
     create_results_file()
@@ -108,10 +121,12 @@ if __name__ == "__main__":
     words, exec_time_reading = read_file(book, stopWords)
     exact_count, exec_time_exact_count = exact_counts(words)
     print(f"Time of reading and processing the book {book} is {exec_time_reading} seconds\n")
-    print(f"Time of Exact counter is {exec_time_exact_count} ")
-    k = 5
-    ssc_count, exec_time_ssc = space_saving_count(words, k)
-    print(f"Time of Space Saving Count {exec_time_ssc}")
-    write_results(words, exact_count, exec_time_exact_count, ssc_count, exec_time_ssc)
-   
-
+    print(f"Time of Exact counter is {exec_time_exact_count}\n")
+    write_results(exact_count, exec_time_exact_count)
+    list_k = [25, 50, 75, 100]
+    for k in list_k:
+        if k > 5:
+            ssc_count, exec_time_ssc = space_saving_count(words, k)
+            print(f"Time of Space Saving Count {exec_time_ssc} with k = {k}")
+            write_results_ssc_count(ssc_count, exec_time_ssc, k)
+    
